bca394375e52ecfdfc49b02680c5ceb8cd57e4b4
romahhzg <romahhzg@gmail.com>
2022-09-26 21:26:53 -0500

    Primer commit

---
+++ src/main/java/isil/jdbc/MainApp.java
@@ -0,0 +1,32 @@
+package isil.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MainApp {
+
+    public static void main(String[] var) throws Exception {
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/EP1";
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection con = DriverManager.getConnection(jdbcUrl, "root", "root");
+
+        Statement st = con.createStatement();
+
+        ResultSet rs = st.executeQuery("select * from Users");
+
+        while(rs.next()){
+            System.out.println(rs.getString("id") + " " +
+                    rs.getString("name") +
+                    " " + rs.getString("lastname") +
+                    " " + rs.getString("age") +
+                    " " + rs.getString("city") +
+                    " " + rs.getString("phone"));
+        }
+
+    }
+}
