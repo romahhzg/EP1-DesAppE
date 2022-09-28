6d5784e085517d0e8a4125d82a538c158b0d6835
romahhzg <romahhzg@gmail,com>
2022-09-27 22:13:02 -0500

    Se crea rama

---
+++ src/main/resources/scripts/scripts1.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Users (
+id int,
+name varchar(255),
+lastname varchar(10),
+age varchar (2),
+city varchar(100),
+phone varchar(9)
+)
+
+select * from Users
+
+INSERT INTO Users
+VALUES (1,'Pablo','Zapata', '28', 'Lima', '961433226');
+
+INSERT INTO Users
+VALUES (2,'Jesus','Cruz', '24', 'Chiclayo', '998524622');
+
+INSERT INTO Users
+VALUES (3,'Luis','Ravello', '34', 'Chincha', '981225910');
+
+INSERT INTO Users
+VALUES (4,'Milagros','Ramirez', '21', 'Lima', '907254805');
+
+INSERT INTO Users
+VALUES (5,'Yelitza','Colachagua', '31', 'Piura', '999894140');
